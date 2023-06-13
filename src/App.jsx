fd90ddc3e8b53852bff84175f682bd9777cd6b2a
nRayen <109584133+nRayen@users.noreply.github.com>
2023-06-13 20:19:14 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./components/Header"
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import './../styles/header.scss'
+
+export const Header = () => {
+  return (
+    <header>
+        <img src="./../../public/vite.svg" alt="Logo" />
+        <nav>
+            <a href="#">Accueil</a>
+            <a href="#">Ordinateurs</a>
+            <a href="#">Abonnements</a>
+            <a href="#">Contact</a>
+        </nav>
+    </header>
+  )
+}
